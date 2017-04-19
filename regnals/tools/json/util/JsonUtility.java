bd5da266defac333b7add3b5a5a139911c513664
Fredrik Martinsson <fredrik.martinsson@ifsworld.com>
2017-04-19 12:45:48 +0200

    Adding functionality to write object and array to file.

--- regnals/tools/json/util/JsonUtility.java
+++ regnals/tools/json/util/JsonUtility.java
@@ -1,9 +1,11 @@
 package regnals.tools.json.util;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.MessageFormat;
 import org.json.JSONArray;
@@ -15,42 +17,70 @@ import org.json.JSONObject;
  */
 public class JsonUtility {
 
-    public static JSONArray getJsonArrayFromFile(String filepath) throws Exception {
-        String json = getJsonStringFromFile(filepath);
-        JSONArray jsona = new JSONArray(json);
-        return jsona;
-    }
+   public static JSONArray getJsonArrayFromFile(String filepath) throws Exception {
+      String json = getJsonStringFromFile(filepath);
+      JSONArray jsona = new JSONArray(json);
+      return jsona;
+   }
 
-    public static JSONObject getJsonObjectFromFile(String filepath) throws Exception {
-        String json = getJsonStringFromFile(filepath);
-        JSONObject jsono = new JSONObject(json);
-        return jsono;
-    }
+   public static JSONObject getJsonObjectFromFile(String filepath) throws Exception {
+      String json = getJsonStringFromFile(filepath);
+      JSONObject jsono = new JSONObject(json);
+      return jsono;
+   }
 
-    public static String getJsonStringFromFile(String filepath) throws Exception {
-        StringBuilder json = new StringBuilder();
-        File file = new File(filepath);
-        if (!file.exists()) {
-            throw new Exception(MessageFormat.format("File {0} not found!", filepath));
-        }
-        readFromFile(file, json);
-        return json.toString();
-    }
+   public static String getJsonStringFromFile(String filepath) throws Exception {
+      StringBuilder json = new StringBuilder();
+      File file = new File(filepath);
+      if (!file.exists()) {
+         throw new Exception(MessageFormat.format("File {0} not found!", filepath));
+      }
+      readFromFile(file, json);
+      return json.toString();
+   }
 
-    private static void readFromFile(File file, java.lang.StringBuilder buffer) throws FileNotFoundException, IOException {
-        FileInputStream fis = new FileInputStream(file);
-        BufferedInputStream bis = new BufferedInputStream(fis);
-        try {
-            readFromStream(bis, buffer);
-        } finally {
-            fis.close();
-        }
-    }
+   public static void putJsonArrayToFile(JSONArray jsona, String filepath) throws IOException {
+      StringBuilder json = new StringBuilder();
+      File file = new File(filepath);
+      json.append(jsona.toString());
+      writeToFile(file, json);
+   }
 
-    private static void readFromStream(BufferedInputStream bis, java.lang.StringBuilder buffer) throws IOException {
-        while (bis.available() > 0) {
-            buffer.append((char) bis.read());
-        }
-    }
+   public static void putJsonObjectToFile(JSONObject jsono, String filepath) throws IOException {
+      StringBuilder json = new StringBuilder();
+      File file = new File(filepath);
+      json.append(jsono.toString());
+      writeToFile(file, json);
+   }
+
+   private static void readFromFile(File file, java.lang.StringBuilder buffer) throws FileNotFoundException, IOException {
+      FileInputStream fis = new FileInputStream(file);
+      BufferedInputStream bis = new BufferedInputStream(fis);
+      try {
+         readFromStream(bis, buffer);
+      } finally {
+         fis.close();
+      }
+   }
+
+   private static void readFromStream(BufferedInputStream bis, java.lang.StringBuilder buffer) throws IOException {
+      while (bis.available() > 0) {
+         buffer.append((char) bis.read());
+      }
+   }
+
+   private static void writeToFile(File file, java.lang.StringBuilder buffer) throws FileNotFoundException, IOException {
+      FileOutputStream fos = new FileOutputStream(file);
+      BufferedOutputStream bos = new BufferedOutputStream(fos);
+      try {
+         writeToStream(bos, buffer);
+      } finally {
+         fos.close();
+      }
+   }
+
+   private static void writeToStream(BufferedOutputStream bos, java.lang.StringBuilder buffer) throws IOException {
+      bos.write(buffer.toString().getBytes());
+   }
 
 }
